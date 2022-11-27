dd0d672a0031de12ce1f617d3fbe35c7383b55b9
carfiguru@hotmail.com <carfiguru@hotmail.com>
2022-11-27 08:05:31 -0600

    initial code push with react framework

---
+++ src/components/About/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+function About() {
+  return (
+    <section id="about" className="aboutme">
+      <h2 id="about" className="aboutme-leftside">About</h2>
+      <p className="aboutme-rightside">
+
+        &emsp;Hello, my name is Bobby Bridges. I am a father, grandfather, soccer fan and independant business owner. I have
+        been in the tech world since I was a kid taking
+        audio gear apart to figure out how it worked and how to make it do different things I wanted to make it do.
+        That enginuity brought me to the audio/video and automation
+        world back in July of 2000. From there I have rose to be one of the leading salesmen at HI-FI FO-FUM.  I've since honed my skills
+        in high end audio, high end video, whole house automation & integration.  And I'm pleased to add top quality network systems and service
+        to the resume.
+        <br></br>
+        <br></br>
+        &emsp;My list of services offered range from almost any technology utilized in the home.
+        My true skill is making technology nice and approachable for almost anyone.  Regardless of age or technical
+        propensity.  Lets explore whats possible and make those tech dreams come true!
+        <br></br>
+        <br></br>
+
+      </p>
+    </section>
+  );
+}
+
+export default About;
---
+++ src/components/Resume/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+function Resume(props) {
+
+  return (
+    <section id="work" className="work">
+      <h2 className="work-leftside">Services</h2>
+      <div >
+        <div className="serv">
+        <br></br>
+        <br></br>        <br></br>
+        <br></br>        <br></br>
+        <br></br>
+        <ul>
+        <li>Home Cinema</li>
+        <li>Whole House Audio</li>
+        <li>Whole House HD Video</li>
+        <li>High Performance Home Network w/Seemless Wifi</li>
+        <li>Reference Video Calibration</li>
+        <li>Home Automation w/Bespoke GUI</li>
+        <li>CD Collection Ripping/Duplication</li>
+        </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Resume;
