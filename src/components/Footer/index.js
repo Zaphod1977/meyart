f1bcbb7bda00257e0274bbbe6e78925399e72b81
Zaphod1977 <carfiguru@hotmail.com>
2025-02-06 16:14:45 -0600

    initial push from new machine

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,7 @@ function Footer() {
   return (
     <footer className='space'>
         <div id="con">
-            <a>Zaphod Bobbybrox</a>
+            <a>Zaphod Bobbybrox lol</a>
             <a href="https://www.instagram.com/zaphod_bobbybrox/?hl=en" target="_blank">Instagram</a>
             <a href="https://open.spotify.com/show/2ip0Ojn7AwzukeawerfPDE" target="_blank">Spotify</a>
         </div>
